b2e17e30cfb97e3e8e7c3516330044dd931fd3a0
Michela Sdino <tuamail@gmail.com>
2025-07-08 16:35:12 +0200

    Aggiunta logica completa MainActivity

--- app/src/main/java/com/example/passafe/MainActivity.kt
+++ app/src/main/java/com/example/passafe/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.passafe
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
-import android.widget.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -46,7 +52,10 @@ class MainActivity : AppCompatActivity() {
         bottoneCopia.setOnClickListener {
             val passwordDaCopiare = testoPassword.text.toString()
             if (passwordDaCopiare.isNotEmpty()) {
-                Toast.makeText(this, "Password copiata: $passwordDaCopiare", Toast.LENGTH_SHORT).show()
+                val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                val clip = ClipData.newPlainText("password", passwordDaCopiare)
+                clipboard.setPrimaryClip(clip)
+                Toast.makeText(this, "Password copiata negli appunti" , Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(this, "Nessuna password da copiare", Toast.LENGTH_SHORT).show()
             }
